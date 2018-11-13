b2b8fdeebddd1b389c0b9184356d53d9f761d43e
Basia Kmiecik <basiakmiecik@gmail.com>
2018-11-13 15:30:06 +0100

    dodanie wyszukiwania

--- src/main/java/com/example/userbase/controller/UserController.java
+++ src/main/java/com/example/userbase/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.MediaSize;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -32,12 +33,38 @@ public class UserController {
         }
         return response;
     }
+    @GetMapping("/znajdz")
+    @ResponseBody
+    //@RequestMapping(value="/find", method = RequestMethod.GET)
+    public String findUser(@RequestParam String imie, @RequestParam String nazwisko, @RequestParam String wiek ) {
+       /* System.out.println("Imię:"+ imie);
+        System.out.println("Nazwisko:"+ nazwisko);
+        System.out.println("Wiek:"+ wiek);*/
+
+        List<User> users = userRepository.getUsers();
+        String fUser = "";
+
+        for (User user : users) {
+            /*System.out.println(user.getFirstName());*/
+            if (!(imie.isEmpty())&&user.getFirstName().equals(imie))
+            {
+                    return fUser = user.toString();
+            } else if (!(nazwisko.isEmpty())&&user.getLastName().equals(imie))
+            {
+                    return fUser = user.toString();
+            } else if (!(wiek.isEmpty())&&user.getAge() == Integer.valueOf(wiek))
+            {
+                    return fUser = user.toString();
+            }
+        }
+        return fUser;
+    }
 
 
 
     @PostMapping("/add")
-    @RequestMapping(value="/add", method = RequestMethod.POST)
-    public String saveUser(String imie, String nazwisko, int wiek) {
+    //@RequestMapping(value="/add", method = RequestMethod.POST)
+    public String saveUser(@RequestParam String imie, @RequestParam String nazwisko, @RequestParam int wiek) {
 
         if(imie.isEmpty() ||nazwisko.isEmpty()|| wiek <= 0) {
             //http://localhost:8080/add?imie=&nazwisko=Rayjko&wiek=30
